1d7ccd22638fe2838c9e89006e49d9d8908b1547
Nihal Singh <nihal.111@gmail.com>
2017-03-25 15:34:16 +0530

    Added basic GET

--- app/src/main/java/ferozepurwale/run/MainActivity.java
+++ app/src/main/java/ferozepurwale/run/MainActivity.java
@@ -1,13 +1,44 @@
 package ferozepurwale.run;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final String TAG = "MainActivity";
+    private static final String GET_SCORE = "http://10.196.16.16:8080/";
+    private static final String SEARCH_URL = "http://www.eventual.co.in/search";
+    private final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            String response = run(GET_SCORE);
+            Log.d(TAG, response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
