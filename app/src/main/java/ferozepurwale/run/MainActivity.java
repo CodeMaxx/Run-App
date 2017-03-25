42446b1e3b6093bdb93401783b7bb9ee94a6a14f
Nihal Singh <nihal.111@gmail.com>
2017-03-25 15:47:52 +0530

    Added basic GET

--- app/src/main/java/ferozepurwale/run/MainActivity.java
+++ app/src/main/java/ferozepurwale/run/MainActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private static final String TAG = "MainActivity";
-    private static final String GET_SCORE = "http://10.196.16.16:8080/";
+    private static final String GET_SCORE = "http://10.196.16.16:8080/name/";
     private static final String SEARCH_URL = "http://www.eventual.co.in/search";
     private final OkHttpClient client = new OkHttpClient();
 
@@ -24,21 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            String response = run(GET_SCORE);
-            Log.d(TAG, response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//        RequestBody body = RequestBody.create(JSON, jsonData);
+//        Log.d(TAG, "My Events JSON = " + jsonData);
 
-
-    String run(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
+        Request request = new com.squareup.okhttp.Request.Builder()
+                .url(GET_SCORE)
                 .build();
 
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(com.squareup.okhttp.Request request, IOException throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(com.squareup.okhttp.Response response) throws IOException {
+                if (!response.isSuccessful())
+                    throw new IOException("Unexpected code " + response);
+                else {
+                    final String jsonData = response.body().string();
+                    Log.d(TAG, "Response from " + GET_SCORE + ": " + jsonData);
+                }
+            }
+        });
+
     }
+
 }
